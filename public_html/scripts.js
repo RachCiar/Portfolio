f9b4d1110fe39ce3c690fdb8f653da8125cd4394
rciarl8967 <rciarl8967@wsacslt37208.suscorp.edu>
2019-04-29 18:55:34 -0400

    4-29-19-Revisions

--- public_html/scripts.js
+++ public_html/scripts.js
@@ -29,8 +29,22 @@ $(function () {
         $('.carousel').carousel('prev');
         return false;
     });
+    // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "0px 5px";
+    document.getElementById("logo").style.fontSize = "15px";
+  } else {
+    document.getElementById("navbar").style.padding = "40px 10px";
+    document.getElementById("logo").style.fontSize = "20px";
+  }
+}
 
 
 })(jQuery);
+
 
 
